1241ef4c6f49059676a36099961ad29a3759f2c5
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2024-09-21 16:04:22 +0530

    implementing the state management for auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,22 +1,30 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { LoginComponent } from "./login/login.component";
-import { CommonModule } from "@angular/common";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { AUTH_STATE_NAME } from './state/auth.selector';
+import { AuthReducer } from './state/auth.reducers';
 
-
-const routes:Routes = [
-    {
-        path:'',
-        children:[
-            {path:'',redirectTo:'login',pathMatch:'full'},
-            {path:'login',component:LoginComponent}
-        ]
-    }
-]
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+    ],
+  },
+];
 
 @NgModule({
-    declarations:[LoginComponent],
-    imports:[ CommonModule,RouterModule.forChild(routes),ReactiveFormsModule,FormsModule]
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(AUTH_STATE_NAME,AuthReducer),
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    FormsModule,
+  ],
 })
 export class AuthModule {}
