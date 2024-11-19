46d1c19e4d223063939462e5fc029626747570de
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2024-11-19 18:44:25 +0530

    changes related to light mode and dark mode functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AppState } from './store/app.state';
 import { Store } from '@ngrx/store';
@@ -12,14 +12,45 @@ import { autoLogin } from './auth/state/auth.actions';
 })
 export class AppComponent implements OnInit {
   title = 'counter-ngrx';
+  theme: 'light' | 'dark' = 'light'; // Default theme
+
   showLoading!:Observable<boolean>
   errorMessage!:Observable<string>
-constructor(private store:Store<AppState>){}
+  isDarkMode = false;
+constructor(private store:Store<AppState>,private renderer: Renderer2){}
 
   ngOnInit(): void {
     this.showLoading = this.store.select(getLoading)
     this.errorMessage = this.store.select(getErrorMessage)
     this.store.dispatch(autoLogin())
-    
+    const savedTheme = localStorage.getItem('theme') as 'light' | 'dark';
+    if (savedTheme) {
+      this.theme = savedTheme;
+    }
+
+    // Apply the theme to <html>
+    this.updateTheme();
   }
-}
+
+  toggleTheme(): void {
+    // Toggle theme
+    this.theme = this.theme === 'light' ? 'dark' : 'light';
+
+    // Save preference in localStorage
+    localStorage.setItem('theme', this.theme);
+
+    // Apply the theme
+    this.updateTheme();
+  }
+
+  private updateTheme(): void {
+    // Dynamically set the theme at the <html> level
+    const htmlElement = document.documentElement;
+    this.renderer.setAttribute(htmlElement, 'data-bs-theme', this.theme);
+  }
+   
+  }
+
+    
+  
+
